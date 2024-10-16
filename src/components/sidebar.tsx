053d7467056ecb5f3cd064722d1b438f7e698eb8
fliyyer <rahmatcube15@gmail.com>
2024-10-17 00:10:43 +0700

    sampai select workspaces

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { Separator } from "./ui/separator"
 import { Navigation } from "./navigation"
+import WorkspaceSwtcher from "./workspace-swtcher"
 
 const Sidebar = () => {
     return (
@@ -9,6 +10,8 @@ const Sidebar = () => {
             <Link href="/">
                 <Image src="/logo.svg" width={164} height={48} alt="logo" />
             </Link>
+            <Separator className="my-4" />
+            <WorkspaceSwtcher />
             <Separator className="my-4" />
             <Navigation />
         </aside>
